f10823ac64bfd5c7e7d9e2f7f189118140e05fdd
subhappy <lars.augensen@gmail.com>
2024-10-28 22:02:44 +0100

    Nikola auto commit.
    Nikola version: 8.3.1

--- conf.py
+++ conf.py
@@ -156,7 +156,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "hyde"
 
 # A theme color. In default themes, it might be displayed by some browsers as
 # the browser UI color (eg. Chrome on Android). Other themes might also use it
@@ -1407,7 +1407,9 @@ WARN_ABOUT_TAG_METADATA = False
 
 # Put in global_context things you want available on all your templates.
 # It can be anything, data, functions, modules, etc.
-GLOBAL_CONTEXT = {}
+GLOBAL_CONTEXT = {
+	 "hyde_subtheme": "theme-base-08"
+}
 GLOBAL_CONTEXT['utterances_config'] = {"issue-term": "pathname", "theme": "github-light", "crossorigin": "anonymous" }
 
 # Add functions here and they will be called with template
